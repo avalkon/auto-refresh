8604f9d1bd031a7d968fc8795f608e857d606055
avalkon <304543037+avalkon@users.noreply.github.com>
2026-09-08 12:25:24 -0400

    Delete auto-refresh directory

--- auto-refresh/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-source /opt/apps/auto-refresh/bin/activate
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-echo "Script directory: $SCRIPT_DIR"
-pip uninstall -r $SCRIPT_DIR/requirements.txt
-deactivate
-rm -r /opt/apps/auto-refresh
-rm /usr/share/applications/auto-refresh.desktop
